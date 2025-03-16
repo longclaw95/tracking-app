8780aebc645fccc39c057f34944ec8ee79de0b3b
longclaw95 <wassimzeddini@gmail.com>
2025-03-16 13:05:04 +0100

    adding task entity class

--- task-app/src/main/java/com/devtiro/task/app/domain/entities/Task.java
+++ task-app/src/main/java/com/devtiro/task/app/domain/entities/Task.java
@@ -1,4 +1,141 @@
 package com.devtiro.task.app.domain.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name= "Tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="due_date")
+    private LocalDateTime due_date;
+
+    @Column(name="status", nullable = false)
+    private TaskStatus status;
+
+    @Column(name="priority", nullable = false)
+    private TaskPriority priority;
+
+    @Column(name="created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name="updated", nullable = false)
+    private LocalDateTime updated;
+
+    public Task() {
+    }
+
+    public Task(UUID id, String title, String description, LocalDateTime due_date, TaskStatus status, TaskPriority priority, LocalDateTime created, LocalDateTime updated) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.due_date = due_date;
+        this.status = status;
+        this.priority = priority;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDue_date() {
+        return due_date;
+    }
+
+    public void setDue_date(LocalDateTime due_date) {
+        this.due_date = due_date;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(title, task.title) && Objects.equals(description, task.description) && Objects.equals(due_date, task.due_date) && status == task.status && priority == task.priority && Objects.equals(created, task.created) && Objects.equals(updated, task.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, due_date, status, priority, created, updated);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", due_date=" + due_date +
+                ", status=" + status +
+                ", priority=" + priority +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
 }
