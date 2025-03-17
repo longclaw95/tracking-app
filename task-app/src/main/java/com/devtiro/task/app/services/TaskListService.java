d15b35c7402e9970d653e63a87c40fa7d28e37eb
longclaw95 <wassimzeddini@gmail.com>
2025-03-17 14:27:54 +0100

    add create task list method

--- task-app/src/main/java/com/devtiro/task/app/services/TaskListService.java
+++ task-app/src/main/java/com/devtiro/task/app/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.devtiro.task.app.services;
 import com.devtiro.task.app.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
